9a00a40be53afcb9f45ef40dda41ccf4f7be10e5
Chao Shin <chao.shin@gmail.com>
2018-04-07 18:30:31 +0800

    IOS11 APP『Swift4』實例說明 — 計算小費或是稅金

--- DemoTip/ViewController.swift
+++ DemoTip/ViewController.swift
@@ -9,7 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var moneyTextField: UITextField!
+    @IBOutlet weak var percentageTextField: UITextField!
+    @IBOutlet weak var featuresNameLabel: UILabel!
+    @IBOutlet weak var ansLabel: UILabel!
+    
+    @IBAction func tapViewPress(_ sender: Any) {    //按畫⾯空⽩白方收鍵盤
+        view.endEditing(true)   // 收鍵盤
+    }
+    
+    func caluclationAns() -> String{    // 計算與判斷結果
+        if let moneyText = moneyTextField.text , let percentageText = percentageTextField.text {    //確認金額與小費是否為Nil
+            if let money = Double(moneyText), let percentage = Double(percentageText){  //確認金額與小費的字串轉為Double是否成功
+                return "\(money * percentage / 100)"    //回傳計算結果，並轉為String
+            }else {
+                return "Error"
+            }
+        }else {
+            return "Error"
+        }
+    }
+    
+    func updateUI(result: String){  // 更新計算結果
+        ansLabel.text = result
+    }
+    
+    func cleanUI() {    // 清除計算結果、輸入金額與百分比
+        ansLabel.text = ""
+        moneyTextField.text = ""
+        percentageTextField.text = ""
+    }
+    
+    @IBAction func caluclationPress(_ sender: Any) {
+        updateUI(result: caluclationAns())  // 呼叫更新計算結果
+        view.endEditing(true)   // 收鍵盤
 
+    }
+    
+    @IBAction func changeFeaturesPress(_ sender: UISegmentedControl) {
+        featuresNameLabel.text = sender.selectedSegmentIndex == 0 ? "小費:" : "稅金："   // 切換功能，所以顯示不同文字
+        cleanUI()   // 清除計算結果、輸入金額與百分比
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
